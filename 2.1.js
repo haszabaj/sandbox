837e81017dae5570a1496063c1a919f871adfa15
Agata Małkiewicz <hushabye92@gmail.com>
2017-04-06 20:11:35 +0200

    Add files via upload

---
+++ 2.1.js
@@ -0,0 +1,5 @@
+var result = "";
+for (i = 1; result.length <= 7; i++) {
+  console.log(result);
+  result += "#";
+}
---
+++ 2.2.js
@@ -0,0 +1,16 @@
+num = 1;
+for (i=0; i<100; i++) {
+  if ((num % 3 == 0) && (num % 5 == 0)) {
+    console.log("FizzBuzz");
+  }
+   else if (num % 3 == 0) {
+    console.log("Fizz");
+  }
+   else if (num % 5 == 0) {
+    console.log("Buzz");
+   }
+  else {
+    console.log(num);
+}
+  	num++;
+}
