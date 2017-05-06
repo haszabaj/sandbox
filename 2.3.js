d9a89d2cb3634083cadcc7deb51c5f5384cbf0f5
Agata Małkiewicz <hushabye92@gmail.com>
2017-05-06 10:35:08 +0200

    Update 2.3.js

--- 2.3.js
+++ 2.3.js
@@ -1,14 +1,9 @@
 var result = "";
-size = 8;
+var size = 8;
 
-for (var l = 0; l < size; l++) {
-    for (var c = 0; c < size; c++) {
-        if ((l + c) % 2 == 0) {
-            result += " ";
-        } 
-        else {
-            result += "#";
-        }
+for (var line = 0; line < size; line++) {
+    for (var column = 0; column < size; column++) {
+        ((line + column) % 2 === 0) ? result += " ": result += "#";
     }
     result += "\n";
 }
