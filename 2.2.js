435efcbab283ea82974b2226ead2f1f4377eaf5b
Agata Małkiewicz <hushabye92@gmail.com>
2017-05-06 10:34:38 +0200

    Update 2.2.js

--- 2.2.js
+++ 2.2.js
@@ -1,16 +1,11 @@
-num = 1;
-for (i = 0; i < 100; i++) {
-    if ((num % 3 == 0) && (num % 5 == 0)) {
-        console.log("FizzBuzz");
-    } 
-    else if (num % 3 == 0) {
-        console.log("Fizz");
-    } 
-    else if (num % 5 == 0) {
-        console.log("Buzz");
-    } 
-    else {
-        console.log(num);
+var output;
+for (var i = 1; i <= 100; i++) {
+    output = "";
+    if (i % 3 === 0) {
+        output += "Fizz";
     }
-    num++;
+    if (i % 5 === 0) {
+        output += "Buzz";
+    }
+    console.log(output || i);
 }
